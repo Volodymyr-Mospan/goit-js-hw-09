2b7a7ef145cd11c8539081423080fd5a6a8fc833
Volodymyr <mospan.volodymyr@gmail.com>
2022-12-24 16:10:48 +0200

    completed task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
+const ref = {
+  body: document.querySelector('body'),
+  btnStart: document.querySelector('[data-start]'),
+  btnStop: document.querySelector('[data-stop]'),
+};
 
+let colorTimerId = null;
+
+ref.btnStart.addEventListener('click', onStartClick);
+ref.btnStop.addEventListener('click', onStopClick);
+
+function onStartClick() {
+  ref.btnStart.setAttribute('disabled', '');
+
+  colorTimerId = setInterval(() => {
+    ref.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+
+function onStopClick() {
+  ref.btnStart.removeAttribute('disabled', '');
+  clearInterval(colorTimerId);
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
